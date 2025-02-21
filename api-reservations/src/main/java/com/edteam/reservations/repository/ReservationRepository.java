86d44f2de1136fb39a0dd92ac908ee34cea7b8de
Nicolás Yepes <nicolasyepes2004@gmail.com>
2025-02-21 10:54:08 -0500

    Se cambió la clase a una interface que hereda de JpaRepository<Clase,PK> y se importaron las clases Reservation del paquete model, y la clase JpaRepository

--- api-reservations/src/main/java/com/edteam/reservations/repository/ReservationRepository.java
+++ api-reservations/src/main/java/com/edteam/reservations/repository/ReservationRepository.java
@@ -1,5 +1,8 @@
 package com.edteam.reservations.repository;
 
+import com.edteam.reservation.model.Reservation
+import org.springframework.data.jpa.repository.JpaRepository
 
-public class ReservationRepository {
+public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+
 }
