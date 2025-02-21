1e97881cbfed31822e8cbe4906389c0e91a2240e
Nicolás Yepes <nicolasyepes2004@gmail.com>
2025-02-21 10:25:53 -0500

    Se agregaron al POM las dependencias de mysql-connector y de spring-data-jpa

---
+++ api-reservations/src/main/java/com/edteam/reservations/dto/ItineraryDTO.java
@@ -0,0 +1,36 @@
+package com.edteam.reservations.dto;
+
+import java.util.List;
+
+public class ItineraryDTO {
+
+    private Long id;
+
+    private List<SegmentDTO> segment;
+
+    private PriceDTO price;
+
+    public List<SegmentDTO> getSegment() {
+        return segment;
+    }
+
+    public void setSegment(List<SegmentDTO> segment) {
+        this.segment = segment;
+    }
+
+    public PriceDTO getPrice() {
+        return price;
+    }
+
+    public void setPrice(PriceDTO price) {
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
