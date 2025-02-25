0cac102f9262838680ae18432b1b79fda81b2c9a
Nicolás Yepes <nicolasyepes2004@gmail.com>
2025-02-25 18:01:52 -0500

    Se agregó la clase Base al paquete model, con la anotación @MappedSuperClass, y se junto todo el código duplicado del atributo id, que estaba en todas las otras clases de este paquete, con sus getters y setters, y las anotaciones propias @Id y @GeneratedValue

--- api-reservations/src/main/java/com/edteam/reservations/model/Base.java
+++ api-reservations/src/main/java/com/edteam/reservations/model/Base.java
@@ -1,2 +1,21 @@
-package com.edteam.reservations.model;public class Base {
+package com.edteam.reservations.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.Id;
+
+@MappedSuperclass
+public class Base {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
