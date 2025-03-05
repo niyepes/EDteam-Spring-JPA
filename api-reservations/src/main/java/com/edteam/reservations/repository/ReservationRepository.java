1a72a0254026257b282fd3fc3c5cef60366d5734
Nicolás Yepes <nicolasyepes2004@gmail.com>
2025-03-04 23:25:49 -0500

    Se agregaron a la interfaz ReservationRepository, los métodos findByCreationDate, findByCreationDateAndPassengersFirstName y findByCreationDateAndPassengersFirstNameAndPassengersLastName, nombrados según el estándar de los métodos custom, y con sus respectivos parámetros

--- api-reservations/src/main/java/com/edteam/reservations/repository/ReservationRepository.java
+++ api-reservations/src/main/java/com/edteam/reservations/repository/ReservationRepository.java
@@ -3,6 +3,17 @@ package com.edteam.reservations.repository;
 import com.edteam.reservations.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    List<Reservation> findByCreationDate (LocalDate localDate);
+
+    List<Reservation> findByCreationDateAndPassengersFirstName (LocalDate localDate, String firstName);
+
+    List<Reservation> findByCreationDateAndPassengersFirstNameAndPassengersLastName (LocalDate localDate, String firstName, String lastName);
+
 
 }
