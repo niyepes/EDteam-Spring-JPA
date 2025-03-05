40403bb313c8eea0f96509b236fbbbfeb2037323
Nicolás Yepes <nicolasyepes2004@gmail.com>
2025-03-05 00:30:54 -0500

    Se agregó en la interface ReservationRepository las constantes con las queries a la base de datos, sobre cada uno de los métodos de la interface; se agrega la anotación @Query que recibe las queries, y a la par de los parámetros de cada método, la anotación @Param, para indicarle a la query de forma dinámica

--- api-reservations/src/main/java/com/edteam/reservations/repository/ReservationRepository.java
+++ api-reservations/src/main/java/com/edteam/reservations/repository/ReservationRepository.java
@@ -2,6 +2,8 @@ package com.edteam.reservations.repository;
 
 import com.edteam.reservations.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -9,11 +11,17 @@ import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    List<Reservation> findByCreationDate (LocalDate localDate);
+    String QUERY_FIND_BY_CREATION_DATE = "SELECT r FROM Reservation r WHERE r.creationDate = :creationDate";
+    String QUERY_FIND_BY_CREATION_DATE_AND_FIRSTNAME = "SELECT r FROM Reservation r JOIN r.passengers p WHERE r.creationDate = :creationDate AND p.firstName = :fistName";
+    String QUERY_FIND_BY_CREATION_DATE_AND_FIRSTNAME_AND_LASTNAME = "SELECT r FROM Reservation r JOIN r.passengers p WHERE r.creationDate = :creationDate AND p.firstName = :fistName AND p.lastName = :lastName";
+    @Query(QUERY_FIND_BY_CREATION_DATE)
+    List<Reservation> findByCreationDate (@Param("creationDate") LocalDate creationDate);
 
-    List<Reservation> findByCreationDateAndPassengersFirstName (LocalDate localDate, String firstName);
+    @Query(QUERY_FIND_BY_CREATION_DATE_AND_FIRSTNAME)
+    List<Reservation> findByCreationDateAndPassengersFirstName (@Param("creationDate")LocalDate localDate,@Param("firstName") String firstName);
 
-    List<Reservation> findByCreationDateAndPassengersFirstNameAndPassengersLastName (LocalDate localDate, String firstName, String lastName);
+    @Query(QUERY_FIND_BY_CREATION_DATE_AND_FIRSTNAME_AND_LASTNAME)
+    List<Reservation> findByCreationDateAndPassengersFirstNameAndPassengersLastName (@Param("creationDate") LocalDate localDate,@Param("firstName") String firstName, @Param("lastName") String lastName);
 
 
 }
