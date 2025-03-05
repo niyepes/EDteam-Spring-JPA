c47a97a3b5bd75062b2740832d9f7cdf1c509c21
Nicolás Yepes <nicolasyepes2004@gmail.com>
2025-03-05 10:46:51 -0500

    Se agregó el package dao con la interface IReservationDao, que declara en abstracto, los métodos que estaban en la clase ReservationService; y una clase llamada ReservationDao que los implementa, y que además tiene la anotación @Repository, para indicar que es una clase persistible, y la anotación @Transactional

--- api-reservations/src/main/java/com/edteam/reservations/dao/IReservationDao.java
+++ api-reservations/src/main/java/com/edteam/reservations/dao/IReservationDao.java
@@ -1,2 +1,21 @@
-package com.edteam.reservations.dao;public interface IReservationDao {
+package com.edteam.reservations.dao;
+
+import com.edteam.reservations.dto.SearchReservationCriteriaDTO;
+import com.edteam.reservations.model.Reservation;
+import java.util.List;
+import java.util.Optional;
+
+public interface IReservationDao {
+
+    List<Reservation> findAll (SearchReservationCriteriaDTO criteria);
+
+    Optional<Reservation> findById (Long id);
+
+    Reservation save (Reservation reservation);
+
+    void deleteById (Long id);
+
+    boolean existsById (Long id);
+
+
 }
--- api-reservations/src/main/java/com/edteam/reservations/dao/ReservationDao.java
+++ api-reservations/src/main/java/com/edteam/reservations/dao/ReservationDao.java
@@ -1,2 +1,38 @@
-package com.edteam.reservations.dao;public class ReservationDao {
+package com.edteam.reservations.dao;
+
+import com.edteam.reservations.dto.SearchReservationCriteriaDTO;
+import com.edteam.reservations.model.Reservation;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Repository
+public class ReservationDao implements IReservationDao{
+    @Override
+    public List<Reservation> findAll(SearchReservationCriteriaDTO criteria) {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Reservation> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Reservation save(Reservation reservation) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return false;
+    }
 }
